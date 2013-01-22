5242d58e0f764c5bba883f1e47a184e5389aa2a5
Paras Kuhad <paras.kuhad@gmail.com>
2013-01-22 15:49:38 +0530

    Substitution rule, writing the exact equation was feeling like a trivia but cleared the beauty of what exactly it meant : Lesson = don't ignore simple and clean

--- number-algorithms/modulo.py
+++ number-algorithms/modulo.py
@@ -21,6 +21,21 @@ def equivalence_classes(N, integer_range):
                 this_class = [ i + k*N for k in integer_range ] # Kudos to list comprehension 
                 print this_class
 
+def imply_substitution_rule( x, x1, y, y1, N ):
+        '''
+        if x===x1 (mod N) and y===y1 (mod N) then :
+                x+y === x1+y1 (mod N)  AND xy = x1y1 (mod N)
+        for example :
+                25 x 3 mod 24 => 1 x 3 mod 24 [ As 25 === 1 mod 24 ]
+                xy mod N => x1y mod N [ As x === x1 mod 24 ] ( here y = y1 if we are following the definition )
+        '''
+        if ( is_x_y_congruent_modulo_N( x, x1, N) and is_x_y_congruent_modulo_N( y, y1, N ) ):
+                if ( is_x_y_congruent_modulo_N ( x+y, x1+y1, N ) and is_x_y_congruent_modulo_N( x*y, x1*y1, N ) ):
+                        return 1
+        
+        return 0
+
+
         
 
 if __name__ == "__main__":
@@ -32,6 +47,10 @@ if __name__ == "__main__":
 
         equivalence_classes(3, range(-5,5))
 
+        print imply_substitution_rule( 25,1, 12, 36, 24) 
+        # We can also test this substituion rule against 'equivalence classes of N within a given integer range Z
+        # i.e. [ ... 25, 1, -23 .... ] or [ ....12, 36, 60.... ] is a snapshot of two equivalence class of 24 with center element being '1' and '12'
+        # and we just used 25,1 and 12,36
 
 '''
 Output :
@@ -41,4 +60,5 @@ Output :
 [-15, -12, -9, -6, -3, 0, 3, 6, 9, 12]
 [-14, -11, -8, -5, -2, 1, 4, 7, 10, 13]
 [-13, -10, -7, -4, -1, 2, 5, 8, 11, 14]
+1
 '''
